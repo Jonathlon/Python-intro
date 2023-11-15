fada5f7361ec7dd1be34fb4941a1f61761091748
Jon Heaney <113617821+Jonathlon@users.noreply.github.com>
2023-11-15 15:38:52 +0000

    Exercise 1.3

---
+++ Exercise-1.3/Practice-Task-3/practice3.py
@@ -0,0 +1,17 @@
+# FOR Loop
+
+for number in range(10, 51, 10):
+    print(number)
+
+print("And we're done!")
+
+# WHILE Loop
+
+number = 10
+while number < 51:
+    print(number)
+    number += 10
+
+print("And we're done!")
+
+
